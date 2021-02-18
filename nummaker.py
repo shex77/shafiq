310ebaadbbe8765b2275312c47f6023e53905d6c
SHAFO <69957344+shex77@users.noreply.github.com>
2021-02-18 22:03:06 +0300

    Add files via upload

---
+++ nummaker.py
@@ -0,0 +1,61 @@
+import random
+import os
+
+
+
+os.system("clear")
+
+aas = """
+
+╭━━━┳╮╱╭┳━━━┳━━━┳━━━╮
+┃╭━╮┃┃╱┃┃╭━╮┃╭━╮┃╭━╮┃
+┃┃╱╰┫┃╱┃┃╰━╯┃┃╱╰┫┃╱┃┃
+┃┃╭━┫┃╱┃┃╭╮╭┫┃╭━┫╰━╯┃
+┃╰┻━┃╰━╯┃┃┃╰┫╰┻━┃╭━╮┃
+╰━━━┻━━━┻╯╰━┻━━━┻╯╱╰╯
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀       
+Am Toola taybata badrws krdni raqa 4mlonraqam ba tekaly
+random asyawkorak drwsaka
+ =============================
+"""
+
+def start():
+	print(aas)
+	a = ("0770","0750","0771","0751","0772","0773")
+	op=open(".txt","w")
+	for x in range(4000000):
+		f = "1234567890"
+		x1 = random.choice(a)
+		x2 = random.choice(f)
+		x3 = random.choice(f)
+		x4 = random.choice(f)
+		x5 = random.choice(f)
+		x6 = random.choice(f)
+		x7 = random.choice(f)
+		x8 = random.choice(f)
+		x9 = random.choice(f)
+		x10 = random.choice(f)
+		x11 = random.choice(f)
+		
+		dd = x1+x2+x3+x4+x5+x6+x7+x8+x9+x10+x11
+		print(dd)
+		op.write(dd+"/n")
+		
+
+		
+def ss():
+	print(aas)
+	print("Tkaya bo das pekrdn [1]Lebda")
+	
+	f = int(input("[×] : "))
+	if f==1:
+		os.system("clear")
+		start()
+	else :
+		print("Tkaya Zhmara[1]Bnwsa")
+		
+ss()
+		
+		
+	
+	
