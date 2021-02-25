5a2461d315e9e73aae06fb12826f145fe3a40ce9
SHAFO <69957344+shex77@users.noreply.github.com>
2021-02-25 22:39:08 +0300

    Create shafiq.py

---
+++ shafiq.py
@@ -0,0 +1,265 @@
+import requests
+
+import uuid
+
+import sys
+
+import os
+
+from random import *
+
+import colorama 
+
+from colorama import *
+
+import time 
+
+import string 
+
+from user_agent import generate_user_agent
+
+import random
+
+ruksI='IdMN1w9spls'
+
+rg = 'D'
+
+#@DIBIBl
+
+h = '_' 
+
+ruks_ = '\033[1;36m'
+
+rf = 'l'
+
+ry = 'B'
+
+ruks__ = '\033[1;36m'
+
+ruksj='VCUHJ2xP1'
+
+tuks1 = 'poiuytrewqasdfghjklmnbvcxz12'
+
+jruks = '\033[1;33m'
+
+_ruks  = '\033[1;31m'
+
+BGreen='\033[1;32m'
+
+BRed ='\033[1;31m'
+
+ruksl='AAGgq9CpeCNh'
+
+#@DIBIBl
+
+token = '1468816115:AAEIp-9MZhDzrAiUtU-52IUSGQB-jnqFfN0'
+
+logo=choice(["""{w}
+
+   ^     . .     ^  
+
+   \ \    V    / /  
+
+   \   \(+ +)/   / {r}({y}Programmer{r}){rr}━━━━━━━━━━{r}({y}ruks{r}){w} 
+
+   \   (  v  )   / {r}({y}Telegram{r}){rr}━━━━━━━━━{r}({y}@{D}{l}{B}{l}{B}{l}{w})
+
+    \____m_m____/   {r}({y}Free tool{r}/{y}RUKS{r}){rr}━━━━━━━{r}({y}V3{r}){w}
+
+   <[___________]>
+
+""".format(r=ruks__,y=jruks,rr=_ruks,w=ruks_,D=rg,l=rf,B=ry),#@DIBIBl
+
+"""                                              {y}                 
+
+@@@  @@@  @@@   @@@@@@   @@@@@@@   @@@@@@   
+
+@@@  @@@@ @@@  @@@@@@@   @@@@@@@  @@@@@@@@  
+
+@@!  @@!@!@@@  !@@         @@!    @@!  @@@  
+
+!@!  !@!!@!@!  !@!         !@!    !@!  @!@  
+
+!!@  @!@ !!@!  !!@@!!      @!!    @!@!@!@!  
+
+!!!  !@!  !!!   !!@!!!     !!!    !!!@!!!!  
+
+!!:  !!:  !!!       !:!    !!:    !!:  !!!  
+
+:!:  :!:  !:!      !:!     :!:    :!:  !:!  
+
+ ::   ::   ::  :::: ::      ::    ::   :::  
+
+:    ::    :   :: : :       :      :   : :                                 
+
+{rr}𖢌{r} Programmer   {rr}:{y}ruks
+
+{rr}𖢌{r} Telegram     {rr}:{y}@{D}{l}{B}{l}{B}{l}
+
+{rr}𖢌{r} Free tool    {rr}:{y}V3{rr}/{y}ruks
+
+{rr}𖢌{r} Last Update  {rr}:{y}N{rr}/{y}2021
+
+""".format(r=ruks__,y=jruks,rr=_ruks,w=ruks_,D=rg,l=rf,B=ry),
+
+"""                                                 {y} 
+
+                        ___                     
+
+                       (   )                    
+
+ ___ .-.     ___  ___   | |   ___       .--.    
+
+(   )   \   (   )(   )  | |  (   )    /  _  \   
+
+ | ' .-. ;   | |  | |   | |  ' /     . .' `. ;  
+
+ |  / (___)  | |  | |   | |,' /      | '   | |  
+
+ | |         | |  | |   | .  '.      _\_`.(___) 
+
+ | |         | |  | |   | | `. \    (   ). '.   
+
+ | |         | |  ; '   | |   \ \    | |  `\ |  
+
+ | |         ' `-'  /   | |    \ .   ; '._,' '  
+
+(___)         '.__.'   (___ ) (___)   '.___.'                  
+
+{rr}\\_{r}Programmer{rr}>{y}ruks
+
+{rr}\\_{r}Telegram{rr}>{y}@{D}{l}{B}{l}{B}{l}
+
+{rr}\\_{r}All-My-Links{rr}>{y}linktr.ee{rr}/{y}m4rktn
+
+{rr}\\_{r}Last-Update{rr}:{y}Nov{rr}/{y}2020
+
+""".format(r=ruks__,y=jruks,rr=_ruks,w=ruks_,D=rg,l=rf,B=ry),
+
+])
+
+print(logo)
+
+print("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+
+print(jruks+'['+_ruks+'1'+jruks+']'+ruks__+' running Checker ;')
+
+print(jruks+'['+_ruks+'2'+jruks+']'+ruks__+' Developer channel ;')
+
+print('')
+
+ruks = input(jruks+'['+_ruks+'+'+jruks+']'+ruks__+' Choose ! -> ;'+BGreen)
+
+id = input(jruks+'['+_ruks+'+'+jruks+']'+ruks__+' ID TELEGRAM ! -> ;'+BGreen)
+
+print("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+
+ru = ('{"account_created": false, "errors": {"email": [{"message": "Too many accounts are using a@gmail.com.", "code": "email_sharing_limit"}], "__all__": [{"message": "Create a password at least 6 characters long.", "code": "too_short_password"}]}, "dryrun_passed": false, "username_suggestions": [], "status": "ok", "error_type": "form_validation_error"}')
+
+ruksz=' 𝐓𝐞𝐥𝐞 :@RUKS3 ☆ @DIBIBl'
+
+def Code_ruks():    
+
+    if ruks =='1':
+
+        Number = 0
+
+        while True:
+
+            user = str("".join(random.choice(tuks1)for i in range(5)))
+
+            us = str("".join(random.choice(h+user)for i in range(5)))            
+
+            url = 'https://www.instagram.com/accounts/web_create_ajax/attempt/'
+
+            heade_ruks={
+
+                'accept': '*/*',
+
+                'accept-encoding': 'gzip, deflate, br',
+
+                'accept-language': 'ar,en-US;q=0.9,en;q=0.8,ar-SA;q=0.7',
+
+                'content-length': '61',
+
+                'content-type': 'application/x-www-form-urlencoded',
+
+                'cookie': 'ig_cb=2; ig_did=BB52B198-B05A-424E-BA07-B15F3D4C3893; mid=YAlcaQALAAHzmX6nvD8dWMRVYFCO; shbid=15012; rur=PRN; shbts=1612894029.7666144; csrftoken=CPKow8myeXW9AuB3Lny0wNxx0EzoDQoI',
+
+                'origin': 'https://www.instagram.com',
+
+                'referer': 'https://www.instagram.com/accounts/emailsignup/',
+
+                'sec-ch-ua': '"Google Chrome";v="87", " Not;A Brand";v="99", "Chromium";v="87"',
+
+                'sec-ch-ua-mobile': '?0',
+
+                'sec-fetch-dest': 'empty',
+
+                'sec-fetch-mode': 'cors',
+
+                'sec-fetch-site': 'same-origin',
+
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36',
+
+                'x-csrftoken': 'CPKow8myeXW9AuB3Lny0wNxx0EzoDQoI',
+
+                'x-ig-app-id': '936619743392459',
+
+                'x-ig-www-claim': 'hmac.AR0Plwj5om112fwzrrYnMNjMLPnyWfFFq1tG7MCcMv5_vN9M',
+
+                'x-instagram-ajax': '72bda6b1d047',
+
+                'x-requested-with': 'XMLHttpRequest'
+
+            } 
+
+#The data shown above are identical Do not touch anything developer Ruks          
+
+            data_ruks={
+
+                'email' : 'a@gmail.com',
+
+                'username': f'{us}',
+
+                'first_name': 'AA',
+
+                'opt_into_one_tap': 'false'
+
+            }
+
+#ruks DIBIBl
+
+            da = requests.post(url,headers=heade_ruks,data=data_ruks).text
+
+            if ru in  da:
+
+                Number+=1
+
+                print(jruks+'['+BGreen+f'{Number}'+jruks+'] Available'+BGreen+f' [{us}]')
+
+                with open('insta-ruks.txt', 'a') as x:
+
+                	x.write(us+ '\n')                	tlg =(f'https://api.telegram.org/bot1468816115:{ruksl}{ruksj}SrS{ruksI}/sendMessage?chat_id={id}&text=\n☆RUKS-هاذة اليوزر متاح انستا ✥\n┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉\n ☆.⍆.user : {us} \n┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉\n ; ruks;{ruksz} ')           	
+
+                	req = requests.post(tlg)             
+
+#@DIBIBl                
+
+            else:
+
+                Number+=1
+
+                print(jruks+'['+BRed+f'{Number}'+jruks+'] Unavailable'+BRed+f'-[{us}]')
+
+if ruks =='2':
+
+	print('5')
+
+#@DIBIBl	
+
+else:
+
+	print('')
+
+Code_ruks()
